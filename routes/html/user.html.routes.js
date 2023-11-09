7739191c30816246ee7cd958c6c5b489085bc4e5
Leah Tillman <leahtillman@Leahs-MacBook-Air.local>
2023-11-09 12:29:36 -0600

    initial structure of folders and files subject to change

---
+++ routes/html/user.html.routes.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+
+// html page for listing all records
+router.get("/users", (req, res) => {
+    //res.sendFile("path to file")
+})
+
+
+// html page for a single record
+router.get("/user", (req, res) => {
+    //res.sendFile("path to file")
+})
+
+module.exports = router;
