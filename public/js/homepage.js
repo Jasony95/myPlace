0d2675ea8a96a3c4cd0c26df34e82cdab97a8333
Jason Yang <jasony24@outlook.com>
2023-11-13 20:02:42 -0600

    Modifiy Homepage

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,7 +1,35 @@
-//handlebars conditionals: login true or false in req.session.loggedIn. Set as variable, pass value into HB. if loggedin, <div>, else <div> for login etc. handled in backend
+// const myModal = document.getElementById('myModal')
+// const myInput = document.getElementById('myInput')
 
-var loginBtn = document.querySelector(".login");
-var logoutBtn = document.querySelector(".logout");
-var userPageBtn = document.querySelector(".favorites");
-var newPlace = document.querySelector(".newPlace");
+// // myModal.addEventListener('shown.bs.modal', () => {
+// //   myInput.focus()
+// // })
 
+// myModal.addEventListener('click', () => {
+//   myInput.focus()
+// })
+
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
