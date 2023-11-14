f5f07c51316425f1c6d0703a76d8adc35638f1fc
Leah Tillman <leahtillman@Leahs-MacBook-Air.local>
2023-11-13 20:07:25 -0600

    fixed minor typo in css

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,7 +1,6 @@
-//handlebars conditionals: login true or false in req.session.loggedIn. Set as variable, pass value into HB. if loggedin, <div>, else <div> for login etc. handled in backend
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
 
-var loginBtn = document.querySelector(".login");
-var logoutBtn = document.querySelector(".logout");
-var userPageBtn = document.querySelector(".favorites");
-var newPlace = document.querySelector(".newPlace");
-
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
