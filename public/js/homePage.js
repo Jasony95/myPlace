b0334627a6f210996b73140b7d71f0a097382286
Leah Tillman <leahtillman@Leahs-MacBook-Air.local>
2023-11-14 11:57:08 -0600

    fixed homepage js

--- public/js/homePage.js
+++ public/js/homePage.js
@@ -1,6 +1,20 @@
-const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('myInput')
 
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
-})
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("myBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
