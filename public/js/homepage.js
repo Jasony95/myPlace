d32682e68e84daed63cb526f7a124febb744e267
Gary T Almes <garytalmes@gmail.com>
2023-11-16 07:11:59 -0500

    Bunch of small revs and login/signup fix.

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,7 +1,9 @@
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
 var span = document.querySelector(".modal-header span")
-
+var addAddressForm = document.querySelector(".formbox")
+var locationName = document.querySelector(".newName")
+var locationAddress = document.querySelector(".newAddress")
 
 buttonHide()
 
@@ -26,6 +28,29 @@ function buttonHide() {
 
 }
 
+addAddressForm?.addEventListener("submit", async function(e){
+  e.preventDefault()
 
+  if( !locationAddress.value.length ){
+    return alert("Please specify your location.")
+  }
 
-
+  try {
+    const query = await fetch("/api/user/location", {
+      method: "PUT",
+      body: JSON.stringify({
+        locationName: locationName.value, 
+        location: locationAddress.value
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    const result = await query.json() 
+    console.log(result)
+    window.location.reload()
+  } catch(err){
+    console.log(err.message)
+    alert("hmmm... something went wrong")
+  }
+})
